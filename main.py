3405b7daa4cff047e9238755f37e5bd18c259ffe
illTushar <gtushar7668@gmail.com>
2024-05-01 00:12:17 +0530

    1.5.24x1

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from schema_and_db_connection.table_schema.database_schema import User
 from schema_and_db_connection.Request.student_request import UserRequest
 from schema_and_db_connection.db_connection.database_connection import SessionLocal, engine, Base
 
+# create db connection
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
 
 # Dependency
 def get_db():
@@ -15,8 +19,6 @@ def get_db():
     finally:
         db.close()
 
-# Create all database tables during application startup
-Base.metadata.create_all(bind=engine)
 
 @app.post("/user_data", status_code=status.HTTP_201_CREATED)
 def input_user_data(request: UserRequest, db: Session = Depends(get_db)):
@@ -26,40 +28,61 @@ def input_user_data(request: UserRequest, db: Session = Depends(get_db)):
     db.refresh(user_data)
     return {"data": request}
 
+
 @app.get("/user_data/all", status_code=status.HTTP_200_OK)
 async def all_user_info(db: Session = Depends(get_db)):
     all_users_info = db.query(User).all()
-    if len(all_users_info) == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data list is empty")
+    if not all_users_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="data list is empty")
     return {"data": all_users_info}
 
+
 @app.get("/user_data/{id}", status_code=status.HTTP_200_OK)
-def specific_user_info(id: int, db: Session = Depends(get_db)):
+def specific_user_info(id, db: Session = Depends(get_db)):
     user_info = db.query(User).filter(User.id == id).first()
     if user_info is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Info not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="info not found")
+    elif not user_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user info list is empty")
     return {"data": user_info}
+
 
 @app.delete("/user_data/all", status_code=status.HTTP_200_OK)
 def delete_all_info(db: Session = Depends(get_db)):
-    delete_all = db.query(User).delete()
-    if delete_all == 0:
+    delete_all = db.query(User)
+    delete_data = []
+    if not delete_all:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no data present")
-    db.commit()
-    return {"message": "Deletion successful", "deleted_count": delete_all}
+    else:
+        for deletion in delete_all:
+            delete_data.append(deletion)
+            db.delete(deletion)
+        db.commit()
+    return {"message": "Deletion successful", "data": delete_data}
+
 
 @app.delete("/user_data/{id}", status_code=status.HTTP_200_OK)
-def delete_specific_user_info(id: int, db: Session = Depends(get_db)):
+def delete_specific_user_info(id, db: Session = Depends(get_db)):
     delete_info = db.query(User).filter(User.id == id).delete(synchronize_session=False)
-    if delete_info == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="ID is not present")
-    db.commit()
-    return {"message": "Deletion successful", "deleted_count": delete_info}
+
+    if delete_info is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id is not present")
+
+    elif not delete_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="list is empty")
+    else:
+        db.commit()
+    return {"message": "deletion successful", "data": delete_info}
+
 
 @app.put("/user_data/{id}", status_code=status.HTTP_200_OK)
-def update_user_info(id: int, user_info: UserRequest, db: Session = Depends(get_db)):
-    update = db.query(User).filter(User.id == id).update(user_info.dict())
-    if update == 0:
+def update_user_info(id, user_info: UserRequest, db: Session = Depends(get_db)):
+    update = db.query(User).filter(User.id == id)
+    if update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
-    db.commit()
+    elif not update:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There is no data")
+    else:
+        update.update(user_info.dict())
+        db.commit()
     return {"message": "Update successful"}
--- schema_and_db_connection/db_connection/database_connection.py
+++ schema_and_db_connection/db_connection/database_connection.py
@@ -1,26 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# Define the base directory for the SQLite database
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SQLITE_URL = "sqlite:///" + os.path.join(BASE_DIR, "DB", "database_table.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./DB/database_table.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-# Define the PostgreSQL database URL
-POSTGRES_URL = "postgresql://user:password@postgresserver/db"
-
-# Get the value of the ENVIRONMENT environment variable
-environment = os.getenv("ENVIRONMENT", "development")
-
-# Use SQLite for development and testing environments, PostgreSQL for production
-if environment == "production":
-    SQLALCHEMY_DATABASE_URL = POSTGRES_URL
-else:
-    SQLALCHEMY_DATABASE_URL = SQLITE_URL
-
-# Create the SQLAlchemy engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
